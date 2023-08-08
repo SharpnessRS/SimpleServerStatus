1e37035191ba662b1358ed39a63fa032dc732d17
SharpnessRS <ars3131arss@gmail.com>
2023-08-08 08:56:48 +0300

    Clean code

--- index.php
+++ index.php
@@ -4,75 +4,8 @@
     <meta charset="utf-8">
     <link rel="shortcut icon" type="image/x-icon" href="">
     <title>Server Status</title>
-    <style>
-        body {
-            background-color: #212121;
-            color: white;
-            text-align: center;
-            font-family: Arial, sans-serif;
-            padding: 203px;
-        }
-
-        h1 {
-            margin-top: 40px;
-            font-size: 24px;
-            letter-spacing: 2px;
-        }
-
-        ul {
-            list-style: none;
-            padding: 0;
-            display: flex;
-            flex-direction: column;
-            align-items: stretch;
-        }
-
-        li {
-            margin-bottom: 20px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-            background-color: #000;
-            padding: 10px;
-            border-radius: 5px;
-        }
-
-        .server-name {
-            font-size: 18px;
-            font-weight: bold;
-            text-align: center;
-            margin-bottom: 10px;
-        }
-
-        .status {
-            font-size: 18px;
-            font-weight: bold;
-            text-align: center;
-        }
-
-        .up {
-            color: #5cb85c;
-        }
-
-        .down {
-            color: #d9534f;
-        }
-
-        .icon {
-            font-size: 24px;
-            margin-left: 10px;
-        }
-
-        .icon.up::before {
-            content: '\2714';
-        }
-
-        .icon.down::before {
-            content: '\2716';
-        }
-    </style>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 </head>
 <body>
     <h1>Server Status</h1>
