11b2043fe3901b3707693a391924ecee9a25b24a
Sharpness <75036963+SharpnessRS@users.noreply.github.com>
2023-11-16 01:37:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <title>Server Status</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-	 <link rel="stylesheet" type="text/css" href="/assets/css/white.css">
-	<!-- <link rel="stylesheet" type="text/css" href="/assets/css/dark.css"> -->
+	<link rel="stylesheet" type="text/css" href="/assets/white.css">
+	<!-- <link rel="stylesheet" type="text/css" href="/assets/dark.css"> -->
 </head>
 <body>
     <div class="container">
